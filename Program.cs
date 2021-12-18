d908c9fdf74c8ab1a99f072442616f1fe2f329b5
Sah19-coder <alievich@gmail.com>
2021-12-18 23:52:50 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+Console.Write("Whats you name?");
+string myname = Console.ReadLine();
+
+DateTime mydate = new DateTime(); // Объявляем переменную с датой и инициализируем
+mydate = DateTime.Now; // присваиваем текущую дату.
+Console.WriteLine(value: $"Привет, {myname}, сегодня {mydate.ToString("dd/MM/yyyy")}.");
+
